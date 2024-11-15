8a679df6e4988d7210c23038539170377b379f22
rev0212 <ra6857@srmist.edu.in>
2024-11-15 18:30:34 +0530

    Add VirtualTryOn Screen (CollectionView)

---
+++ CellPractice3/ApprealCollectionViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  ApprealCollectionViewCell.swift
+//  CellPractice3
+//
+//  Created by admin29 on 15/11/24.
+//
+
+import UIKit
+
+class ApprealCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
--- CellPractice3/VitonPreviewViewController.swift
+++ CellPractice3/VitonPreviewViewController.swift
@@ -4,36 +4,36 @@
 //
 //  Created by admin29 on 05/11/24.
 //
-//
-
 
 import UIKit
 
 class VitonPreviewViewController: UIViewController {
     
-    // MARK: - IBOutlets
-    @IBOutlet weak var firstImageView: UIImageView!
-    @IBOutlet weak var secondImageView: UIImageView!
-    @IBOutlet weak var shareButton: UIButton!
-    
-    @IBOutlet weak var previewImage: UIImageView!
     // MARK: - Properties
+    var receivedPhoto: UIImage?
+    var receivedApparel: UIImage?
+    
+    @IBOutlet weak var selectedPhoto: UIImageView!
+    @IBOutlet weak var selectedApparel: UIImageView!
+    @IBOutlet weak var shareButton: UIButton!
+    @IBOutlet weak var previewImage: UIImageView!
+    
     private var currentGesture: UITapGestureRecognizer?
-    let image = UIImage(systemName: "square.and.arrow.up.fill")
+    let shareIcon = UIImage(systemName: "square.and.arrow.up.fill")
     
     // MARK: - Lifecycle Methods
     override func viewDidLoad() {
         super.viewDidLoad()
         setupUI()
-        shareButton.setBackgroundImage(image, for: .normal)
+        shareButton.setBackgroundImage(shareIcon, for: .normal)
         setupImageSelection()
-        
+        loadReceivedImages()
     }
     
     // MARK: - UI Setup
     private func setupUI() {
         // Configure image views
-        [firstImageView, secondImageView].forEach { imageView in
+        [selectedPhoto, selectedApparel].forEach { imageView in
             imageView?.contentMode = .scaleAspectFit
             imageView?.clipsToBounds = true
             imageView?.backgroundColor = .systemGray6
@@ -50,15 +50,32 @@ class VitonPreviewViewController: UIViewController {
         let tapGesture1 = UITapGestureRecognizer(target: self, action: #selector(imageViewTapped(_:)))
         let tapGesture2 = UITapGestureRecognizer(target: self, action: #selector(imageViewTapped(_:)))
         
-        firstImageView.isUserInteractionEnabled = true
-        secondImageView.isUserInteractionEnabled = true
+        selectedPhoto.isUserInteractionEnabled = true
+        selectedApparel.isUserInteractionEnabled = true
         
-        firstImageView.addGestureRecognizer(tapGesture1)
-        secondImageView.addGestureRecognizer(tapGesture2)
+        selectedPhoto.addGestureRecognizer(tapGesture1)
+        selectedApparel.addGestureRecognizer(tapGesture2)
+    }
+    
+    private func loadReceivedImages() {
+        if let photo = receivedPhoto {
+            selectedPhoto.image = photo
+            selectedPhoto.backgroundColor = .clear
+        } else {
+            selectedPhoto.image = nil
+            selectedPhoto.backgroundColor = .systemRed
+        }
+        
+        if let apparel = receivedApparel {
+            selectedApparel.image = apparel
+            selectedApparel.backgroundColor = .clear
+        } else {
+            selectedApparel.image = nil
+            selectedApparel.backgroundColor = .systemGreen
+        }
     }
     
     // MARK: - Button Actions
-    
     @IBAction func shareButtonTapped(_ sender: Any) {
         handleShareButtonTapped()
     }
@@ -73,42 +90,32 @@ class VitonPreviewViewController: UIViewController {
             return
         }
         
-        // Set up activity view controller
         let activityViewController = UIActivityViewController(activityItems: [image], applicationActivities: nil)
-        activityViewController.popoverPresentationController?.sourceView = self.view // so that iPads won't crash
-        
-        // Exclude some activity types from the list (optional)
+        activityViewController.popoverPresentationController?.sourceView = self.view
         activityViewController.excludedActivityTypes = [UIActivity.ActivityType.airDrop, .postToFacebook]
-        
-        // Present the view controller
         self.present(activityViewController, animated: true, completion: nil)
     }
-
     
     // MARK: - Image Selection
     @objc private func imageViewTapped(_ gesture: UITapGestureRecognizer) {
         currentGesture = gesture
         
         let actionSheet = UIAlertController(title: "Select Photo",
-                                          message: "Choose a source",
-                                          preferredStyle: .actionSheet)
+                                            message: "Choose a source",
+                                            preferredStyle: .actionSheet)
         
-        // Camera action
         if UIImagePickerController.isSourceTypeAvailable(.camera) {
             actionSheet.addAction(UIAlertAction(title: "Camera", style: .default) { [weak self] _ in
                 self?.presentImagePicker(sourceType: .camera)
             })
         }
         
-        // Photo Library action
         actionSheet.addAction(UIAlertAction(title: "Photo Library", style: .default) { [weak self] _ in
             self?.presentImagePicker(sourceType: .photoLibrary)
         })
         
-        // Cancel action
         actionSheet.addAction(UIAlertAction(title: "Cancel", style: .cancel))
         
-        // For iPad support
         if let popoverController = actionSheet.popoverPresentationController {
             popoverController.sourceView = view
             popoverController.sourceRect = CGRect(x: view.bounds.midX, y: view.bounds.midY, width: 0, height: 0)
@@ -129,8 +136,8 @@ class VitonPreviewViewController: UIViewController {
     // MARK: - Helper Methods
     private func showAlert(title: String, message: String) {
         let alert = UIAlertController(title: title,
-                                    message: message,
-                                    preferredStyle: .alert)
+                                      message: message,
+                                      preferredStyle: .alert)
         alert.addAction(UIAlertAction(title: "OK", style: .default))
         present(alert, animated: true)
     }
@@ -139,10 +146,9 @@ class VitonPreviewViewController: UIViewController {
 // MARK: - UIImagePickerControllerDelegate
 extension VitonPreviewViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     func imagePickerController(_ picker: UIImagePickerController,
-                             didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+                               didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         guard let selectedImage = info[.editedImage] as? UIImage else { return }
         
-        // Determine which image view was tapped
         if let gesture = currentGesture,
            let tappedImageView = gesture.view as? UIImageView {
             tappedImageView.image = selectedImage
@@ -155,6 +161,3 @@ extension VitonPreviewViewController: UIImagePickerControllerDelegate, UINavigat
         picker.dismiss(animated: true)
     }
 }
-
-
-
